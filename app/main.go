8ec82d53624fbac485242b68d1ddb081274cc6c6
Mateusz Kurzyna <lolc44t@gmail.com>
2025-06-10 22:37:54 +0200

    CZ2

--- app/main.go
+++ app/main.go
@@ -12,7 +12,14 @@ var _ = fmt.Fprint
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
+
 }
