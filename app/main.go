3627115a9683daed8a1b38985647ff12042761d6
Mateusz Kurzyna <lolc44t@gmail.com>
2025-06-11 21:13:23 +0200

    pn5

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Fprint
@@ -12,12 +13,31 @@ func main() {
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		query, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
-	}
+		command := strings.Fields(query[:len(query)-1])
 
+		commandLen := len(command)
+
+		if commandLen == 0 {
+			fmt.Println("No command entered")
+			continue
+		}
+
+		if command[0] == "exit" {
+			if commandLen == 1 {
+				fmt.Println("Missing parameter")
+				continue
+			}
+
+			if command[1] == "0" {
+				os.Exit(0)
+			} else {
+				fmt.Println("Invalid parameter, use 0 to exit")
+			}
+		}
+	}
 }
