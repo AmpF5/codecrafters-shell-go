2134a5afc55bdbe9471353958653535c68f8c3bf
Mateusz Kurzyna <lolc44t@gmail.com>
2025-06-11 21:29:06 +0200

    iz3

--- app/main.go
+++ app/main.go
@@ -18,7 +18,9 @@ func main() {
 			panic(err)
 		}
 
-		command := strings.Fields(query[:len(query)-1])
+		query = query[:len(query)-1]
+
+		command := strings.Fields(query)
 
 		commandLen := len(command)
 
@@ -39,6 +41,11 @@ func main() {
 			} else {
 				fmt.Println("Invalid parameter, use 0 to exit")
 			}
+		case "echo":
+			echoMsg := strings.SplitAfterN(query, command[0], 2)
+			trimmedMsg := strings.TrimSpace(echoMsg[1])
+
+			fmt.Println(trimmedMsg)
 		default:
 			fmt.Printf("%s: command not found\n", command[0])
 		}
